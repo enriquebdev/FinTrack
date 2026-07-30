752c4205c8ff88fed828cb97cfb97094eb4c9164
EnriqueBernardo <enriquebdev@gmail.com>
2026-07-29 23:46:24 -0300

    feat: cria lógica para adicionar e renderizar novas transações

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,81 @@
+const saldoTela = document.querySelector("#saldo");
+const receitasTela = document.querySelector("#receitas");
+const despesasTela = document.querySelector("#despesas");
 const transacoes = [
     {
+        descricao: "Salário",
         tipo: "Receita",
         valor: 3500
     },
     {
+        descricao: "Mercado",
         tipo: "Despesa",
-        valor: 250
+        valor: 450
     },
     {
+        descricao: "Internet",
         tipo: "Despesa",
         valor: 100
+    },
+    {
+        descricao: "Freelance",
+        tipo: "Receita",
+        valor: 800
     }
 ];
+
 let saldo = 0;
+let receitas = 0;
+let despesas = 0;
 
 transacoes.forEach(transacao => {
 
     if (transacao.tipo === "Receita") {
+        receitas += transacao.valor;
         saldo += transacao.valor;
     } else {
+        despesas += transacao.valor;
         saldo -= transacao.valor;
     }
 
 });
 
-const saldoTela = document.querySelector("#saldo");
+saldoTela.textContent = "R$ " + saldo;
 
-saldoTela.textContent = `R$ ${saldo}`;
+receitasTela.textContent = "R$ " + receitas;
+
+despesasTela.textContent = "R$ " + despesas;
+
+
+const lista = document.querySelector("#lista-transacoes");
+
+transacoes.forEach(transacao => {
+
+    const html = `
+        <div class="transacao">
+
+            <h4>${transacao.descricao}</h4>
+
+            <p>${transacao.tipo}</p>
+
+            <span>R$ ${transacao.valor}</span>
+
+        </div>
+    `;
+
+    lista.innerHTML += html;
+
+});
+const botao = document.querySelector("#adicionar");
+
+botao.addEventListener("click", function(){
+
+    const descricao = document.querySelector("#descricao").value;
+
+    const valor = document.querySelector("#valor").value;
+
+    const tipo = document.querySelector("#tipo").value;
+
+
+
+});
